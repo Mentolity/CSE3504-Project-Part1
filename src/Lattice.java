862837b3b2d86979fc64062e05c474f5df8ac0a4
Mentolity <oneilkyle123@aol.com>
2017-03-30 07:03:17 -0400

    Inital Commit
    Apologies for the cludge in the main class. May refactor at some point.
    Regardless it shouldn't effect the visualizing aspect.

---
+++ src/Lattice.java
@@ -0,0 +1,170 @@
+import java.util.ArrayList;
+
+public class Lattice{
+	private ArrayList<Point> vistedPoints = new ArrayList<Point>(); //using arrayList instead of boolean[][] for infinite lattice case
+	private int xPos; //current x position in lattice
+	private int yPos; //current y position in lattice
+	private int dim; //max dim of lattice
+	private int pathLength = 0; 
+	private boolean isInfinite; //whether the lattice is infinite or not
+	
+	//vars for Practice 2
+	private int xStart;
+	private int yStart;
+	
+	public Lattice(){
+		this(0, true);
+	}
+	
+	public Lattice(int dimension, boolean isInfinite){
+		this.isInfinite = isInfinite;
+		dim = dimension;
+		xPos = dim/2;
+		yPos = dim/2;
+		vistedPoints.add(new Point(xPos, yPos));
+		xStart = xPos;
+		yStart = yPos;
+	}
+	
+	public int getPathLength(){
+		return pathLength;
+	}
+	
+	
+	///////////methods for practice 2
+	public int xStart(){
+		return xStart;
+	}	
+	public int yStart(){
+		return yStart;
+	}
+	
+	public int xMax(){
+		int max = xStart;
+		for(Point p : vistedPoints){
+			if(p.getX() > max)
+				max = p.getX();
+		}
+		return max;
+	}
+	public int xMin(){
+		int min = xStart;
+		for(Point p : vistedPoints){
+			if(p.getX() < min)
+				min = p.getX();
+		}
+		return min;
+	}
+	
+	public int yMax(){
+		int max = yStart;
+		for(Point p : vistedPoints){
+			if(p.getY() > max)
+				max = p.getY();
+		}
+		return max;
+	}
+	public int yMin(){
+		int min = yStart;
+		for(Point p : vistedPoints){
+			if(p.getY() < min)
+				min = p.getY();
+		}
+		return min;
+	}
+	
+	public int xEnd(){
+		return vistedPoints.get(vistedPoints.size()-1).getX();
+	}
+	public int yEnd(){
+		return vistedPoints.get(vistedPoints.size()-1).getY();
+	}
+	///////////
+	
+	public boolean walk(){ //return true if a valid path was taken
+		//System.out.println("X: " + xPos + " Y: " + yPos);
+		int x = xPos;
+		int y = yPos;
+		double r = Math.random();
+		if(r < 0.25){
+			y++; //up
+		}else if(r < 0.50){
+			y--; //down
+		}else if(r < 0.75){
+			x--; //left
+		}else{
+			x++; //right
+		}
+		if(!visted(x,y) && (!outOfBounds(x, y) || isInfinite)){
+			xPos = x;
+			yPos = y;
+			vistedPoints.add(new Point(xPos, yPos));
+			pathLength++;
+			return true;
+		}else if(!stuck()){ //if not stuck keep trying till we take one of the available paths path
+			walk();
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean stuck(){
+		//check if its possible to go in each direction
+		if(!visted(xPos+1,yPos) && (!outOfBounds(xPos+1, yPos) || isInfinite)){
+			return false;
+		}
+		if(!visted(xPos-1,yPos) && (!outOfBounds(xPos-1, yPos) || isInfinite)){
+			return false;
+		}
+		if(!visted(xPos,yPos+1) && (!outOfBounds(xPos, yPos+1) || isInfinite)){
+			return false;
+		}
+		if(!visted(xPos,yPos-1) && (!outOfBounds(xPos, yPos-1) || isInfinite)){
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean outOfBounds(int x, int y){//returns true if the given dimension is out of bounds
+		if((x > dim || y > dim) || (x < 0 || y < 0))
+			return true;
+		else
+			return false;
+	}
+	
+	private boolean visted(int x, int y){//returns true if the point x,y has already been visited
+		Point point = new Point(x, y);
+		for(Point p : vistedPoints){
+			if(p.equals(point))
+				return true;
+		}
+		return false;
+	}
+	
+	
+	private class Point{
+		int x;
+		int y;
+		
+		public Point(int xPos, int yPos){
+			x = xPos;
+			y = yPos;
+		}
+		
+		private int getX(){
+			return x;
+		}
+		private int getY(){
+			return y;
+		}
+		
+		public boolean equals(Point p) {
+			if(x == p.getX() && y == p.getY())
+				return true;
+			else
+				return false;
+		}
+	}
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,170 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Practice 1: ");
+		Practice1();
+		System.out.println("\nPractice 2: ");
+		Practice2();
+		
+	}
+
+	public static void Practice1(){ //Bad copy pasta is bad
+		ArrayList<Integer> listOfPathLengths = new ArrayList<Integer>();
+		
+		Lattice lattice;
+		lattice = new Lattice(100, false);
+		while(lattice.walk()); //walk until stuck
+		listOfPathLengths.add(lattice.getPathLength());
+		
+		System.out.println("1. Calculate the path length (the number of steps travelled until being trapped).");
+		System.out.println("	Path Length: " + mean( listOfPathLengths));
+
+		for(int i=0; i<100; i++){
+			lattice = new Lattice(100, false);
+			while(lattice.walk()); //walk until stuck
+			listOfPathLengths.add(lattice.getPathLength());
+		}
+		System.out.println("2. Run the simulation 100 times and calculate the path length at each time. Calculate:");
+		System.out.println("	Average Path Length: " + mean( listOfPathLengths));
+		System.out.println("	Std Path Length: " + standardDeviation(listOfPathLengths));
+		
+		for(int i=0; i<100; i++){
+			lattice = new Lattice(1000, false);
+			while(lattice.walk()); //walk until stuck
+			listOfPathLengths.add(lattice.getPathLength());
+		}
+		System.out.println("3. Increase the lattice size to 1000×1000 and answer the same questions as in point 2");
+		System.out.println("	Average Path Length: " + mean(listOfPathLengths));
+		System.out.println("	Std Path Length: " + standardDeviation(listOfPathLengths));
+	}
+
+	public static void Practice2(){//icky yucky copy pasta but refactoring would take more time and the assignment isn't graded on best practices
+		Lattice lattice;
+		
+		lattice = new Lattice(); //new infinite lattice
+		for(int i=0; i<5000; i++){//take 5000 walks
+			if(!lattice.walk())//if were stuck there's no point in continuing to walk
+				break;
+		}
+		System.out.println("1. What is the distance between the start point and the end point?");
+		System.out.println("	Distance: " + latticeDistance(lattice));
+		
+		System.out.println("2. What is the lattice size?");
+		System.out.println("	Size: " + latticeSize(lattice));
+		
+		ArrayList<Integer> listOfDistances;
+		ArrayList<Integer> listOfSizes;
+		////////////////////////////////////////////////////////////////////////////////////////////////////////
+		listOfDistances = new ArrayList<Integer>();
+		listOfSizes = new ArrayList<Integer>();
+		for(int x=0; x<10; x++){//run sim 10x
+			lattice = new Lattice(); //new infinite lattice
+			for(int i=0; i<5000; i++){//take 5000 walks
+				if(!lattice.walk())//if were stuck there's no point in continuing to walk
+					break;
+			}
+			listOfDistances.add(latticeDistance(lattice)); //reset the lists
+			listOfSizes.add(latticeSize(lattice));
+		}
+		
+		System.out.println("3. Repeat the simulation 10 times and calculate:");
+		System.out.println("	Distance Mean: " + mean(listOfDistances));
+		System.out.println("	Distance Median: " + median(listOfDistances));
+		System.out.println("	Distance Variance: " + variance(listOfDistances));
+		System.out.println("	Distance Standard Deviation: " + standardDeviation(listOfDistances));
+		System.out.println("	Size Mean: " + mean(listOfSizes));
+		System.out.println("	Size Median: " + median(listOfSizes));
+		System.out.println("	Size Variance: " + variance(listOfSizes));
+		System.out.println("	Size Standard Deviation: " + standardDeviation(listOfSizes));
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////////////
+		listOfDistances = new ArrayList<Integer>(); //reset the lists
+		listOfSizes = new ArrayList<Integer>();
+		for(int x=0; x<100; x++){//run sim 100x
+			lattice = new Lattice(); //new infinite lattice
+			for(int i=0; i<5000; i++){//take 5000 walks
+				if(!lattice.walk())//if were stuck there's no point in continuing to walk
+					break;
+			}
+			listOfDistances.add(latticeDistance(lattice));
+			listOfSizes.add(latticeSize(lattice));
+		}
+		
+		System.out.println("4. Run the simulation 100 times and answer the same questions as in point 3.");
+		System.out.println("	Distance Mean: " + mean(listOfDistances));
+		System.out.println("	Distance Median: " + median(listOfDistances));
+		System.out.println("	Distance Variance: " + variance(listOfDistances));
+		System.out.println("	Distance Standard Deviation: " + standardDeviation(listOfDistances));
+		System.out.println("	Size Mean: " + mean(listOfSizes));
+		System.out.println("	Size Median: " + median(listOfSizes));
+		System.out.println("	Size Variance: " + variance(listOfSizes));
+		System.out.println("	Size Standard Deviation: " + standardDeviation(listOfSizes));
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+		listOfDistances = new ArrayList<Integer>(); //reset the lists
+		listOfSizes = new ArrayList<Integer>();
+		for(int x=0; x<1000; x++){//run sim 1000x
+			lattice = new Lattice(); //new infinite lattice
+			for(int i=0; i<5000; i++){//take 5000 walks
+				if(!lattice.walk())//if were stuck there's no point in continuing to walk
+					break;
+			}
+			listOfDistances.add(latticeDistance(lattice));
+			listOfSizes.add(latticeSize(lattice));
+		}
+		
+		System.out.println("5. Run the simulation 1000 times and answer the same questions as in point 3.");
+		System.out.println("	Distance Mean: " + mean(listOfDistances));
+		System.out.println("	Distance Median: " + median(listOfDistances));
+		System.out.println("	Distance Variance: " + variance(listOfDistances));
+		System.out.println("	Distance Standard Deviation: " + standardDeviation(listOfDistances));
+		System.out.println("	Size Mean: " + mean(listOfSizes));
+		System.out.println("	Size Median: " + median(listOfSizes));
+		System.out.println("	Size Variance: " + variance(listOfSizes));
+		System.out.println("	Size Standard Deviation: " + standardDeviation(listOfSizes));
+		
+		
+		System.out.println("6. Write a comment on how the results change with changing the number of simulations.");
+	}
+	
+	
+	public static int latticeDistance(Lattice lattice){
+		return Math.abs(lattice.xStart() - lattice.xEnd()) + Math.abs(lattice.yStart() - lattice.yEnd());
+	}
+	public static int latticeSize(Lattice lattice){
+		return (lattice.xMax() - lattice.xMin()) * (lattice.yMax() - lattice.yMin());
+	}
+	
+	public static double median(ArrayList<Integer> list){
+		Collections.sort(list);
+		if(list.size() % 2 == 0){
+			return (list.get((list.size()/2)) + list.get((list.size()/2) - 1))/2;
+		}else{
+			return list.get((list.size()-1)/2);
+		}
+	}
+	
+	public static double mean(ArrayList<Integer> list){
+		int sum = 0;
+		for(Integer i : list){
+			sum += i;
+		}
+		return (double)sum/(double)list.size();
+	}
+	
+	public static double variance(ArrayList<Integer> list){
+		double mean = mean(list);
+		double temp = 0;
+		for(Integer i : list){
+			temp += (i-mean)*(i-mean);
+		}
+		return temp/list.size();
+	}
+	
+	public static double standardDeviation(ArrayList<Integer> list){
+		return Math.sqrt(variance(list));
+	}
+}
